79fc1d130d028ad9ba59a54627be15515be185d3
Jan Koupil <jan.koupil@delta-skola.cz>
2021-11-18 10:22:42 +0100

    2021-11-18 - Formulare, http metody, PRG

---
+++ 05-Forms/01-form-demo/01-form-demo.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="registrace.php" method="get">
+    <label for="jmeno"> Jméno:</label> <input type="text" id="jmeno" name="name" placeholder="vaše jméno" /><br />
+    E-mail: <input type="email" name="email" placeholder="váš email" /><br />
+    <label>Pamatuj si mne: <input type="checkbox" name="remember" /></label><br />
+    <input type="submit" value="Odeslat" />
+</form>
+
+<form method='get' action="registrace.php" >
+    <input type="checkbox" name="jidlo[pizza][]" value="šunková" />Šunková<br />
+    <input type="checkbox" name="jidlo[pizza][]" value="hawaii" />Hawaii<br />
+    <input type="checkbox" name="jidlo[pizza][]" value="quatro" />Čtyři roční období<br />
+    <input type="submit" value="odeslat" />
+</form>
+
+
+</body>
+</html>
---
+++ 05-Forms/01-form-demo/registrace.php
@@ -0,0 +1,8 @@
+<?php
+
+header("Location: cil.php?uspech=1");
+exit;
+
+var_dump($_GET);
+
+var_dump($_POST);
